ece73b34bb206bd2bcd0ecc44d69749ae459fced
TheodoreGillot <tgillot@albertschool.com>
2025-02-23 17:01:52 +0000

    working on keyword sell analysis

--- bin/data-cleaning.py
+++ bin/data-cleaning.py
@@ -1,52 +1,28 @@
 import pandas as pd
 import configparser
+import os
 
-class DataCleaner:
-    def __init__(self, config_path):
-        config = configparser.ConfigParser()
-        config.read(config_path)
-        self.product_df_path = config['DEFAULT'].get('product_df', fallback=None)
-        self.client_df_path = config['DEFAULT'].get('client_df', fallback=None)
-        self.transac_df_path = config['DEFAULT'].get('transac_df', fallback=None)
-        self.load_data()
+config = configparser.ConfigParser()
+config.read('../cfg/config.ini')
 
-    def load_data(self):
-        self.product_df = pd.read_csv(self.product_df_path, sep=',')
-        self.client_df = pd.read_csv(self.client_df_path, sep=';')
-        self.transac_df = pd.read_csv(self.transac_df_path, sep=';')
+data_dir = config['DEFAULT'].get('data_dir')
 
-    def clean_client_data(self):
-        self.client_df.drop_duplicates(inplace=True)
-        self.client_df.fillna({
-            'sku_description': 'Unknown',
-            'marketing_color': 'Unknown',
-            'main_material': 'Unknown',
-            'collection': 'Unknown',
-            'aesthetic_line': 'Unknown'
-        }, inplace=True)
+product_df_path = os.path.join(data_dir, config['DEFAULT'].get('combined_product', fallback=None))
+client_df_path = os.path.join(data_dir, config['DEFAULT'].get('combined_client'))
+transac_df_path = os.path.join(data_dir, config['DEFAULT'].get('combined_transac'))
 
-    def clean_product_data(self):
-        if 'week;nationality;gender;universe;macro_family;is_reachable;age;is_big_client;store_type_label;store_zone;clients;items_bought' in self.product_df.columns:
-            self.product_df[['week', 'nationality', 'gender', 'universe', 'macro_family', 'is_reachable', 'age', 'is_big_client', 'store_type_label', 'store_zone', 'clients', 'items_bought']] = self.product_df['week;nationality;gender;universe;macro_family;is_reachable;age;is_big_client;store_type_label;store_zone;clients;items_bought'].str.split(';', expand=True)
-            self.product_df.drop(columns=['week;nationality;gender;universe;macro_family;is_reachable;age;is_big_client;store_type_label;store_zone;clients;items_bought'], inplace=True)
-        self.product_df.drop_duplicates(inplace=True)
+product = pd.read_csv(product_df_path, sep=',')
+client = pd.read_csv(client_df_path, sep=';')
+transac = pd.read_csv(transac_df_path, sep=';')
 
-    def clean_transac_data(self):
-        if 'week;product_id;store_type_label;country;count_distinct_transaction;product_quantity' in self.transac_df.columns:
-            self.transac_df[['week', 'product_id', 'store_type_label', 'country', 'count_distinct_transaction', 'product_quantity']] = self.transac_df['week;product_id;store_type_label;country;count_distinct_transaction;product_quantity'].str.split(';', expand=True)
-            self.transac_df.drop(columns=['week;product_id;store_type_label;country;count_distinct_transaction;product_quantity'], inplace=True)
-        self.transac_df.drop_duplicates(inplace=True)
-
-    def save_cleaned_data(self):
-        self.client_df.to_csv('/media/data/lvmh/cleaned_client.csv', index=False)
-        self.product_df.to_csv('/media/data/lvmh/cleaned_product.csv', index=False)
-        self.transac_df.to_csv('/media/data/lvmh/cleaned_transac.csv', index=False)
-
-    def clean_all_data(self):
-        self.clean_client_data()
-        self.clean_product_data()
-        self.clean_transac_data()
-        self.save_cleaned_data()
-
-cleaner = DataCleaner('../cfg/config.ini')
-cleaner.clean_all_data()
+print(product.info())
+print("-"*50)
+print(client.info())
+print("-"*50)
+print(transac.info())
+print("-"*50)
+print(client.head())
+print("-"*50)
+print(transac.head())
+print("-"*50)
+print(product.head())
